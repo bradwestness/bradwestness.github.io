bec5a27adff4e7eb7265db29f60a1ed1dcb7afcc
Brad Westness <brad.westness@carvana.com>
2022-02-03 13:23:34 -0600

    cookie warning

--- scripts/site.js
+++ scripts/site.js
@@ -1,70 +1,47 @@
 ﻿(function ($) {
 
-    var toggleStyle = function (elementSelector, nightCss, dayCss) {
-        var styleElement = $(elementSelector)[0];
-        var current = styleElement.href;
+    var getCookie = function (key) {
+        var cookie = key + "=";
+        var decoded = decodeURIComponent(document.cookie);
+        var ca = decoded.split(';');
 
-        if (current.indexOf(nightCss) >= 0) {
-            current = current.replace(nightCss, dayCss);
-        } else if (current.indexOf(dayCss) >= 0) {
-            current = current.replace(dayCss, nightCss);
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+
+            while (c.charAt(0) === ' ') {
+                c = c.substring(1);                
+            }
+
+            if (c.indexOf(cookie) === 0) {
+                return c.substring(cookie.length, c.length);
+            }
         }
 
-        styleElement.href = current;
-        saveTheme(elementSelector, current);
+        return "";
     }
 
-    var toggleBootswatch = function () {
-        toggleStyle(
-            "link#bootswatch-style",
-            "content/bootswatch-darkly.min.css",
-            "content/bootswatch-flatly.min.css"
-        );
+    var setCookie = function(key, value, expiryDays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (expiryDays * 24 * 60 * 60 * 1000));
+
+        var expires = "expires=" + d.toUTCString();
+        document.cookie = key + "=" + value + ";" + expires + ";path=/";
     }
 
-    var togglePrism = function () {
-        toggleStyle(
-            "link#prism-style",
-            "content/prism.night.css",
-            "content/prism.default.css"
-        );
-    }
+    var cookieAlert = $("#cookie-alert");
+    var acceptCookies = $("#accept-cookies");
+    var cookieName = "acceptCookies";
 
-    var toggleTheme = function() {
-        toggleBootswatch();
-        togglePrism();
-    }
-
-    var saveTheme = function (elementSelector, value) {
-        if (window.localStorage && window.localStorage.setItem) {
-            window.localStorage.setItem(elementSelector, value);
+    if (cookieAlert){
+        cookieAlert.offsetHeight;
+        if (!getCookie(cookieName)) {
+            cookieAlert.removeClass("d-none");
         }
     }
 
-    var getSavedTheme = function (elementSelector) {
-        if (window.localStorage && window.localStorage[elementSelector]) {
-            return window.localStorage[elementSelector];
-        }
-
-        return null;
-    }
-
-    var getCurrentTheme = function (elementSelector) {
-        var styleElement = $(elementSelector)[0];
-        return styleElement.href;
-    }
-
-    var bootswatchSelector = "link#bootswatch-style";
-    var savedTheme = getSavedTheme(bootswatchSelector);
-
-    if (savedTheme && savedTheme !== getCurrentTheme(bootswatchSelector)) {
-        toggleTheme();
-    }
-
-    $("a#theme-toggle").click(function (click) {
-        toggleTheme();
-        click.preventDefault();
-        return false;
+    acceptCookies.click(function(click) {
+        setCookie(cookieName, true, 365);
+        cookieAlert.addClass("d-none");
     });
 
     $(".post-content img").addClass("img-fluid w-100");
